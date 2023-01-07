bace795785f748cce2d6f1bbf5746131545397d9
BowTiedHeron <bowtiedheron@protonmail.com>
2023-01-07 16:56:55 +0100

    update og data

--- pages/_document.js
+++ pages/_document.js
@@ -1,11 +1,19 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html>
       <Head>
-        <meta property="og:title" content="GPT-3 Writer" key="title"/>
-        <meta property="og:description" content="build with buildspace" key="description"/>
+        <meta
+          property="og:title"
+          content="Prompt Engineering for Boomers"
+          key="title"
+        />
+        <meta
+          property="og:description"
+          content="Experience a New Way to Learn Prompt Engineering with AI Coaching"
+          key="description"
+        />
         <meta
           property="og:image"
           content="https://cdn.buildspace.so/courses/gpt3-writer/project-og.jpg"
@@ -17,5 +25,5 @@ export default function Document() {
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
