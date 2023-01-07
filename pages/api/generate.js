d74dd4b057d0ba6a613cbb1536f2bf62af4967cc
BowTiedHeron <bowtiedheron@protonmail.com>
2023-01-07 16:51:21 +0100

    add basePromptPrefix

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -6,16 +6,21 @@ const configuration = new Configuration({
 
 const openai = new OpenAIApi(configuration);
 
-const basePromptPrefix = "";
+const basePromptPrefix = `
+Suggest a more effective prompt based on A.I. algorithms. Please also provide insights into why certain prompts may be more effective than others. 
+
+user prompt:
+`;
+
 const generateAction = async (req, res) => {
   // Run first prompt
   console.log(`API: ${basePromptPrefix}${req.body.userInput}`);
 
   const baseCompletion = await openai.createCompletion({
     model: "text-davinci-003",
-    prompt: `${basePromptPrefix}${req.body.userInput}`,
-    temperature: 0.7,
-    max_tokens: 250,
+    prompt: `${basePromptPrefix}${req.body.userInput}\n`,
+    temperature: 0.8,
+    max_tokens: 500,
   });
 
   const basePromptOutput = baseCompletion.data.choices.pop();
