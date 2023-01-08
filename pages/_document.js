fa8b4769afbb819793e234f554308a22e330447b
BowTiedHeron <bowtiedheron@protonmail.com>
2023-01-08 10:00:04 +0100

    change og image

--- pages/_document.js
+++ pages/_document.js
@@ -16,7 +16,9 @@ export default function Document() {
         />
         <meta
           property="og:image"
-          content="https://cdn.buildspace.so/courses/gpt3-writer/project-og.jpg"
+          content="https://external-preview.redd.it/hfuRA8PDnoG_yJMXSSJNvFKE9mw3_yOgvEFbgPsM9gE.jpg?auto=webp&s=1b7182206a93b98daa9caf1c3fad1b3049ec7dd8"
+          // backup below
+          // https://i.guim.co.uk/img/media/3aab8a0699616ac94346c05f667b40844e46322f/0_123_5616_3432/master/5616.jpg?width=620&quality=45&dpr=2&s=none
         />
         <meta name="twitter:card" content="summary_large_image"></meta>
       </Head>
